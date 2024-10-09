1d7b70253acbfd92b65e31b94c27fd608afa5a8d
Lorenzo <leccher@eng.it>
2024-10-09 04:24:00 +0200

    Update bluetoothctl.py
    Added type to parameters and methods, and also the feature interpreting device output.

--- bluetoothctl.py
+++ bluetoothctl.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # BluetoothCTL Wrapper
 import time
 import pexpect
@@ -18,8 +19,7 @@ logger = logging.getLogger("Bluettooth_module")
 class Bluetoothctl:
     """A wrapper for bluetoothctl utility."""
     
-    
-    def __init__(self,log_level=logging.WARNING,clean_output:bool=False):
+    def __init__(self,clean_output:bool=True,log_level=logging.WARNING):
         self.__expected_matched=None
         self.__output=[]
         self.__logger = logging.getLogger("Bluetoothctl")
@@ -40,19 +40,19 @@ class Bluetoothctl:
             self.__logger.debug(f"expected_matched={self.__expected_matched}")
             self.__add_output(self.__process.before)
 
-    def __add_output(self,out:str):
+    def __add_output(self,out:str)->None:
         if(self.__clean_outout):
             clean_out=self.clean_output(out)
-            self.__logger.debug(f"Adding to output:[{clean_out}]")
+            self.__logger.debug(f"Adding to output:[{clean_out.encode("raw_unicode_escape")}]")
             self.__output.append(clean_out)
         else:
             self.__logger.debug(f"Adding to output:[{out}]")
             self.__output.append(out)
 
-    def getLogger(self):
+    def getLogger(self)->logging.Logger:
         return self.__logger
 
-    def send(self, command:str, expected:list[str]=[], pause:int=3):
+    def send(self, command:str, expected:list[str]=[], pause:int=3)->None:
         """ This method is the core of the class. It sends command to the bluetoothctl process and undetand when an answer arrived """
         self.__expected_matched=None
         self.__process.send(f"{command}\n")
@@ -72,54 +72,57 @@ class Bluetoothctl:
             self.__logger.debug(f"expected_matched={self.__expected_matched}")
             self.__add_output(self.__process.before)
             
-    def get_output(self, *args, **kwargs):
+    def get_output(self, *args, **kwargs)->str:
         """ Runs a command in bluetoothctl prompt, return output as a list of lines."""
         try:
             self.send(*args, **kwargs)
         except Exception as e:
             raise e   
-        return self.__process.before #self.read_last_output()
+        return self.read_last_output() #self.read_last_output()
     
-    def get_last_match(self):
+    def get_last_match(self)->str:
         """ Gets last string matched the expected output """
         return self.__expected_matched
     
-    def set_clean_output(self,clean:bool):
+    def set_clean_output(self,clean:bool)->None:
         """ Sets if outut is cleaned from now or not """
         self.__clean_output=clean
 
-    def clean_output(self,output:str):
+    def clean_output(self,output:str)->str:
         """ Cleans output from "dirty characters" used by console to embellish the output """
         # crude but effective
         # remove "blue part"
-        output=re.compile("\r\n\\x1b\\[\\?2004h\\x1b\\[0;94m\\[[^\\]]+\\]\\x1b\\[0m").sub("",output)
-        output=re.compile("\\s\\x1b\\[\\??[0-9a-zA-z;]+\\s*").sub("",output)
-        return output
+        output=re.compile("\\x1b\\[\\?[0-9a-zA-Z;]+\\x1b\\[[0-9a-zA-Z;]+[^\\]]+\\]\\x1b\\[[0-9a-zA-Z;]+").sub("",output)
+        # And this remove some other unuseful unicode character
+        output=re.compile("\\[?\\x1b\\[\\??[\\?0-9a-zA-Z;\r\n]+\\]?").sub("",output)
+        # Then trim the string and remove \r chars
+        # Should remain just \n,this should not be a problem
+        return output.strip().replace("\r","")
          
-    def read_last_output(self):
+    def read_last_output(self)->str:
         return self.__output[-1]
 
-    def read_full_output(self):
+    def read_full_output(self)->list[str]:
         """ Returns all output product """
         return self.__output
 
-    def command_help(self):
+    def command_help(self)->str:
         """ Gives back help command. Using explained command will guide to write other features wrapped."""
         return self.get_output("help")
 
-    def command_list(self):
+    def command_list(self)->str:
         """ Gives back list of connector availables."""
         return self.get_output("list")
         
-    def command_devices_connected(self):
+    def command_devices_connected(self)->str:
         """ Gives sback devices connected."""
         return self.get_output("devices Connected")
 
-    def command_devices_paired(self):
+    def command_devices_paired(self)->str:
         """ Gives sback devices paired."""
         return self.get_output("devices Paired")
 
-    def command_pair_device(self,dev_mac:str):
+    def command_pair_device(self,dev_mac:str)->str:
         """ Unpairs device with specific mac address. Returning true if device is paired, false if not"""
         expected=["Pairing successful","Device %s not available" % dev_mac]
         out=self.get_output("pair %s" % dev_mac,expected)
@@ -128,7 +131,7 @@ class Bluetoothctl:
         else:
             return False
 
-    def command_trust_device(self,dev_mac:str):
+    def command_trust_device(self,dev_mac:str)->str:
         """ Trusts device with specific mac address. Returning true if device is trusted, false if not"""
         expected=["Device has been removed","Device %s not available" % dev_mac]
         out=self.get_output("remove %s" % dev_mac,expected)
@@ -137,7 +140,7 @@ class Bluetoothctl:
         else:
             return False    
             
-    def command_connect_device(self,dev_mac:str):
+    def command_connect_device(self,dev_mac:str)->str:
         """ Connects device with specific mac address. Returning true if device is connected, false if not"""
         expected=["Connection successful","Device %s not available" % dev_mac]
         out=self.get_output("connect %s" % dev_mac,expected)
@@ -146,7 +149,7 @@ class Bluetoothctl:
         else:
             return False
             
-    def command_disconnect_device(self,dev_mac:str):
+    def command_disconnect_device(self,dev_mac:str)->str:
         """ Removes device with specific mac address from list of connected, trusted and paired devices. Returning true if device was disconnected, false if not"""
         expected=["Device has been removed","Device %s not available" % dev_mac]
         out=self.get_output("remove %s" % dev_mac,expected)
@@ -155,7 +158,7 @@ class Bluetoothctl:
         else:
             return False
 
-    def command_scan_on(self,dev_mac:str):
+    def command_scan_on(self,dev_mac:str)->str:
         """ Start scanning and searching for specific mac address"""
         expected=[dev_mac,"Discovery started"]
         out=self.get_output("scan on",expected,5)
@@ -164,19 +167,52 @@ class Bluetoothctl:
         else:
             return False
     
-    def command_scan_off(self):
+    def command_scan_off(self)->None:
         """ Stop scanning """
         out=self.get_output("scan off",["Discovery stopped"])
 
 
-def force_reconnect_device(dev_mac:str,log_level=logging.WARNING):
+def parse_device_info(info_string:str):
+    """Parse a string corresponding to a device."""
+    print(info_string.encode("raw_unicode_escape"))
+    device = {}
+    block_list = ["[\x1b[0;", "removed"]
+    string_valid = not any(keyword in info_string for keyword in block_list)
+
+    if string_valid:
+        try:
+            device_position = info_string.index("Device")
+        except ValueError:
+            pass
+        else:
+            if device_position > -1:
+                attribute_list = info_string[device_position:].split(" ", 2)
+                device = {
+                    "mac_address": attribute_list[1],
+                    "name": attribute_list[2]
+                }
+
+    return device
+
+def force_reconnect_device(dev_mac:str,log_level=logging.WARNING)->None:
     # I search if my device is paired
-    bt=Bluetoothctl(log_level)
+    bt=Bluetoothctl(True,log_level)
     dc=bt.command_devices_paired()
     dev_paired=re.search(dev_mac,dc)
+    # If someone needs info about devices...
+    #if dev_paired:
+    #    lines=dc.split("\n")
+    #    dev_info={'mac_address':dev_mac,'name':''}
+    #    for line in lines:
+    #        if re.search(dev_mac,line):
+    #            dev_info=parse_device_info(line)
+    #    
     if dev_paired:
         bt.command_disconnect_device(dev_mac)
-    res=input("Press enter when device %s is pairing ..." % dev_mac)
+    res=input(f"Would you like to reconnect the {dev_mac} device?\nPress y when device {dev_mac} is pairing ...")
+    if(not (res=="y" or res=="Y")):
+        print("Stop connecting %s!" % dev_mac)
+        return
     # start scanning
     bt.command_scan_on(dev_mac)
     # Pairing devices
